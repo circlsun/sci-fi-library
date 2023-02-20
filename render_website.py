6e2754b290e0daf77d5841ad36ea3171c4012ea8
Matveev Andrey <116314099+circlsun@users.noreply.github.com>
2023-02-20 21:45:42 +0300

    Add auto pagination

--- render_website.py
+++ render_website.py
@@ -2,6 +2,7 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 import os
+from math import ceil
 from livereload import Server
 from more_itertools import chunked
 
@@ -9,24 +10,31 @@ os.makedirs('pages', exist_ok=True)
 
 
 def rebuild():
-    with open("json/books_description.json", "r") as file:
-        books = json.load(file)
-    columns = 2
-    pages = 5
-    books_in_row = list(chunked(books, columns))
-    books_in_pages = list(chunked(books_in_row, pages))
-
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
+
+    with open("json/books_description.json", "r") as file:
+        books = json.load(file)
+
+    columns = 2
+    books_per_page = 10
+    pages = ceil((len(books) / columns) / books_per_page)
+
+    books_in_row = list(chunked(books, columns))
+    books_in_pages = list(chunked(books_in_row, pages))
+    count_pages = len(books_in_pages)
+
     template = env.get_template('template.html')
     for num, books_in_page in enumerate(books_in_pages):
-        rendered_page = template.render(books_in_page=books_in_page)
+        rendered_page = template.render(
+            books_in_page=books_in_page,
+            count_pages=count_pages
+        )
 
-        with open(f'pages/index{num}.html', 'w', encoding="utf8") as file:
+        with open(f'pages/index{num + 1}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
-            print("Site rebuilt")
 
 
 rebuild()
