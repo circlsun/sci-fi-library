4ad416645cadcfaf904b787bfbc16cce735c1a46
Matveev Andrey <circlsun@gmail.com>
2023-02-26 11:14:45 +0300

    Rename variables

--- render_website.py
+++ render_website.py
@@ -6,8 +6,6 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
-os.makedirs('pages', exist_ok=True)
-
 
 def rebuild():
     env = Environment(
@@ -16,29 +14,33 @@ def rebuild():
     )
 
     with open('json/books_description.json', 'r') as file:
-        books = json.load(file)
+        books_description = json.load(file)
 
     columns = 2
     books_per_page = 10
-    pages = ceil((len(books) / columns) / books_per_page)
+    pagination_size = ceil((len(books_description) / columns) / books_per_page)
 
-    books_in_row = list(chunked(books, columns))
-    books_in_pages = list(chunked(books_in_row, pages))
-    total_pages = len(books_in_pages)
+    book_cards_in_row = list(chunked(books_description, columns))
+    book_cards_in_pages = list(chunked(book_cards_in_row, pagination_size))
+    pages_amount = len(book_cards_in_pages)
 
     template = env.get_template('template.html')
-    for book_index, books_in_page in enumerate(books_in_pages, start=1):
+    for book_index, books_in_page in enumerate(book_cards_in_pages, start=1):
         rendered_page = template.render(
             books_in_page=books_in_page,
-            total_pages=total_pages,
+            pages_amount=pages_amount,
             current_page=book_index
         )
 
         with open(f'pages/index{book_index}.html', 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
+def main():
+    os.makedirs('pages', exist_ok=True)
+    rebuild()
+    server = Server()
+    server.watch('template.html', rebuild)
+    server.serve(root='.')
 
-rebuild()
-server = Server()
-server.watch('template.html', rebuild)
-server.serve(root='.')
+if __name__ == '__main__':
+    main()
